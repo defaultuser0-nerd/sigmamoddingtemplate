97aba85ca59c8be98ce7bf1a385908526e941ea6
defaultuser0 <axolotlslmao@gmail.com>
2024-12-04 19:27:18 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BepInEx;
+using sigmamodtemplate;
+using UnityEngine;
+using Utilla;
+using Newtilla;
+
+namespace sigmamodtemplate
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void Start()
+		{
+            Newtilla.Newtilla.OnJoinModded += OnModdedJoin;
+            Newtilla.Newtilla.OnLeaveModded += OnModdedLeave;
+            HarmonyPatches.ApplyHarmonyPatches();
+			GorillaTagger.OnPlayerSpawned(Initialized);
+		}
+
+		void OnEnable()
+		{
+			// u enable it idiot
+		}
+
+		void OnDisable()
+		{
+            // u disable it idiot
+		}
+
+		void Initialized(object sender, EventArgs e)
+		{
+			/* code runs right after your player loads */
+		}
+
+		void Update()
+		{
+			/* runs every frame */
+		}
+
+
+		public void OnModdedJoin(string gamemode)
+		{
+			/* it will right even if its enabled/disabled */
+
+			inRoom = true;
+		}
+
+		public void OnModdedLeave(string gamemode)
+		{
+            /* code will go bye bye here */
+            /* it will right even if its enabled/disabled */
+
+            inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace sigmamodtemplate
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.urname.sigmamodtemplate";
+		public const string Name = "sigmamodtemplate";
+		public const string Version = "1.0.0";
+	}
+}
